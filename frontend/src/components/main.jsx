41c7234dc406c4bc469c2ae415ea88d57bde37ef
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2025-03-01 14:27:52 +0000

    feat: Finished main page + set up mobile page (#2)

---
+++ frontend/src/components/main.jsx
@@ -0,0 +1,52 @@
+// Important Imports
+import React from 'react';
+
+// Style sheet
+import "../styles/main.css"
+
+// Global Styles
+import "../styles/global.css"
+
+// Image imports
+import Pringles from "../images/pringles.png" 
+import Cheetos  from "../images/cheetos.png" 
+import Taki     from "../images/taki.png" 
+
+function Main() {
+    return (
+        <div className="main-page-container">
+            <div className="main-page-photo-container">
+
+                <img src={Pringles} alt="Pringles" className="main-page-photo-pringles" />
+                <img src={Cheetos}  alt="Cheetos"  className="main-page-photo-cheetos"  /> 
+                <img src={Taki}     alt="Taki"     className="main-page-photo-taki"     />
+                
+            </div>
+
+            <div className="main-page-content-container">
+
+                <div className="vertical-line"></div>
+
+                <div className="main-page-content-text-container">
+                    <h1>
+                        Feeling obese?
+
+                        <br />
+                        Let's try get you back on TRACK!
+                    </h1>
+
+                    <p1>
+                        We are not some knockoff! We are 100% original and we will help you get back on track. How do you think
+                        you can get from one place to another? Let's find out.
+                    </p1>
+
+                    <br />
+                    <button class="button-24" role="button">Get tracking!!!</button>
+                </div>
+            
+            </div>
+        </div>
+    );
+}
+
+export default Main;
